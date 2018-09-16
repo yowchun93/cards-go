f11ffdec727e639ae5ed5f75b335dfda3ee44b71
yc yap <yowchun93@gmail.com>
2018-09-16 12:34:56 +0800

    card game implemented in GoLang

---
+++ deck.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+// create a new type of 'deck'
+type deck []string
+
+// receiver functions are like methods in OOP languages
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
+
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of"+suit)
+		}
+	}
+	return cards
+}
